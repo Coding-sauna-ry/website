0a249edbbf41935042b57cb33bc8fb996f2cd0eb
Juubes <grumpt1@gmail.com>
2022-04-13 19:01:46 +0300

    Prepare for development

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,27 +2,35 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Coding sauna website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Coding sauna`,
+    siteUrl: `https://codingsauna.fi`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-datocms',
-    options: {
-      "apiToken": ""
-    }
-  }, "gatsby-plugin-postcss", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-datocms",
+    //   options: {
+    //     apiToken: "",
+    //   },
+    // },
+    "gatsby-plugin-postcss",
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    // "gatsby-transformer-sharp",
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: "./src/images/",
+    //   },
+    //   __key: "images",
+    // },
+  ],
 };
 
 export default config;
