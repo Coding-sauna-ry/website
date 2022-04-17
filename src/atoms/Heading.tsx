57bdd5c3a720e31b4fdf10226ac1bf52afa5a512
Juubes <grumpt1@gmail.com>
2022-04-17 15:10:30 +0300

    Heading component done

---
+++ src/atoms/Heading.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { FC } from "react";
+import clsx from "clsx";
+
+type HeadingProps = {
+  size: 1 | 2 | 3;
+  color: "primary" | "secondary";
+};
+
+const Heading: FC<HeadingProps> = (props) => {
+  let sizeClasses;
+  switch (props.size) {
+    case 1:
+      sizeClasses = "text-3xl";
+    case 2:
+      sizeClasses = "text-2xl";
+    case 2:
+      sizeClasses = "text-xl";
+  }
+
+  let colorClasses;
+  switch (props.color) {
+    case "primary":
+      colorClasses = "text-black";
+    case "secondary":
+      colorClasses = "text-white";
+  }
+
+  return <h1 className={clsx(sizeClasses, colorClasses)}>{props.children}</h1>;
+};
+
+export default Heading;
